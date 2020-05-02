047048c5b9e5bd4c0de9c1bb6ddc731e34b1cc32
Hans Engelbrecht <hengelbrecht@godaddy.com>
2020-05-02 16:26:41 -0700

    completed required portion of homework

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
 
+from scipy import stats
+
+
 #################################################
 # Database Setup
 #################################################
@@ -36,8 +39,9 @@ def welcome():
     return (
         f"Available Routes:<br/>"
         f"/api/v1.0/rain<br/>"
-        # f"/api/v1.0/stations<br/>"
-        # f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
     )
 
 @app.route("/api/v1.0/rain")
@@ -46,7 +50,7 @@ def precipitation():
     session = Session(engine)
 
     """Return a list of all passenger names"""
-    # Query all passengers
+    # Query all rain data 
     results = session.query(mm.date, mm.station, mm.prcp, mm.tobs).order_by(mm.date).all()
 
     session.close()
@@ -66,6 +70,107 @@ def precipitation():
         all_rain.append(rain_dict)
 
     return jsonify(all_rain)
+
+@app.route("/api/v1.0/stations")
+def stations():
+     # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query Station data
+    stations = session.query(Station.id, Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation).all()
+    
+    session.close()
+
+    all_stations = []
+    for id, station, name, latitude, longitude, elevation in stations:
+        stations_dict = {}
+        stations_dict["id"] = id
+        stations_dict["station"] = station
+        stations_dict["name"] = name
+        stations_dict["latitude"] = latitude
+        stations_dict["longitude"] = longitude
+        stations_dict["elevation"] = elevation
+        all_stations.append(stations_dict)
+
+    return jsonify(all_stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query observed temp data
+    temp_data = session.query(mm.station, mm.date, mm.tobs).filter(mm.station == 'USC00519281').filter(mm.date >= '2016-08-18' ).order_by(mm.date).all()
+
+    session.close()
+
+    all_temps = []
+    for station, date, tobs in temp_data:
+        temp_dict = {}
+        temp_dict["station"] = station
+        temp_dict["date"] = date
+        temp_dict["tobs"] = tobs
+        all_temps.append(temp_dict)
+
+    return jsonify(all_temps)
+
+@app.route("/api/v1.0/<start>")
+def calc_temps(start):
+    """TMIN, TAVG, and TMAX for a list of dates.
+    
+    Args:
+        start (string): A date string in the format %Y-%m-%d
+        
+    Returns:
+        TMIN, TAVE, and TMAX
+    """
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    temp_calc = session.query(func.min(mm.tobs), func.max(mm.tobs), func.avg(mm.tobs)).\
+        filter(mm.date >= start).all()
+
+    session.close()
+
+    temp_end = []
+    for temp_min, temp_max, temp_avg in temp_calc:
+        temp_dict = {}
+        temp_dict["Min temp"] = temp_min
+        temp_dict["Max temp"] = temp_max
+        temp_dict["Agv temp"] = temp_avg
+        temp_end.append(temp_dict)
+
+    
+    return jsonify(temp_end)
+
+@app.route("/api/v1.0/<start>/<end>")
+def calc_date_range(start, end):
+    """TMIN, TAVG, and TMAX for a list of dates.
+    
+    Args:
+        start (string): A date string in the format %Y-%m-%d
+        end (string): A date string in the format %Y-%m-%d
+        
+    Returns:
+        TMIN, TAVE, and TMAX
+    """
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    date_range = session.query(func.min(mm.tobs), func.max(mm.tobs), func.avg(mm.tobs)).\
+        filter(mm.date >= start).filter(mm.date <= end).all()
+
+    session.close()
+
+    temp_start_end = []
+    for temp_min, temp_max, temp_avg in date_range:
+        temp_dict = {}
+        temp_dict["Min temp"] = temp_min
+        temp_dict["Max temp"] = temp_max
+        temp_dict["Agv temp"] = temp_avg
+        temp_start_end.append(temp_dict)
+
+    return jsonify(temp_start_end)
 
 
 if __name__ == '__main__':
